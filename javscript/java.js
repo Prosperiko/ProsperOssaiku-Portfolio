2e19a5d1b07646535cfddcd61e13c15dee4fba99
Prosper Ossaiku <prosperossaiku@gmail.com>
2025-07-15 15:06:46 -0700

    changes

--- javscript/java.js
+++ javscript/java.js
@@ -16,3 +16,34 @@ navLinks.forEach(link => {
         closeSidebar()
     })
 })
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const fadeInElements = document.querySelectorAll(".fade-in");
+    const slidInElements = document.querySelectorAll(".slid-in");
+    const scalInElements = document.querySelectorAll(".scal-in");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            } else {
+                entry.target.classList.remove("show"); // Remove the class when scrolling up
+            }
+        });
+    }, {
+        threshold: 0.1 // Trigger when 10% of the element is visible
+    });
+
+    fadeInElements.forEach(element => {
+        observer.observe(element);
+    });
+    slidInElements.forEach(element => {
+        observer.observe(element);
+    });
+    scalInElements.forEach(element => {
+        observer.observe(element);
+    });
+});
